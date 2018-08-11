3ee7352d595fe645911ec718e5e1982518d19a37
JackWesleyNelson <nelsonw948@msn.com>
2018-08-11 15:38:17 -0600

    Working on planet generation min distance

--- Assets/Scripts/PlanetGenerator.cs
+++ Assets/Scripts/PlanetGenerator.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class PlanetGenerator : MonoBehaviour {
     public PlanetGenerator Instance { get; private set; }
-    public int Iterations { get; private set; } = 5;
-    public int PlanetChildCount = 3;
+    public int Iterations { get; private set; } = 20;
+    public int PlanetChildCount = 6;
 
     [SerializeField]
     private InventoryItemsDistributor Distributor;
     [SerializeField]
     private LayerMask planetLayerMask;
         
-    private float minRadiusFromPlanets = .8f;
+    private float minRadiusFromPlanets = 1.2f;
     private float maxRadiusFromLastPlanet;
-    private float maxRadiusFromLastPlanetDefault = 1.5f;
+    private float maxRadiusFromLastPlanetDefault = 4.2f;
 
     private float minSizeMult = .2f;
     private float maxSizeMult= .3f;
@@ -26,6 +26,8 @@ public class PlanetGenerator : MonoBehaviour {
 
     private GameObject planetHierarchy;
     private Dictionary<Planet, GameObject> planetDict;
+
+    private bool isSpawning = false;
 
     public void Start() {
         if (!Instance) {
@@ -45,13 +47,15 @@ public class PlanetGenerator : MonoBehaviour {
     IEnumerator RespawnOnTimer() {
         while (true) {
             RespawnPlanets();
-            yield return new WaitForSeconds(2.5f);
+            yield return new WaitForSeconds(5f);
         }
     }
 
     private void RespawnPlanets() {
+        if (isSpawning) {
+            StopAllCoroutines();
+        }
         maxRadiusFromLastPlanet = maxRadiusFromLastPlanetDefault;
-
         //Destroy whatever old planets existed.
         DestroyImmediate(planetHierarchy);
         planetDict.Clear();
@@ -68,10 +72,57 @@ public class PlanetGenerator : MonoBehaviour {
         newPlanet.layer = LayerMask.NameToLayer("Planet");
         planetDict.Add(originPlanet, newPlanet);
         //Using the origin as a parent, begin spawning new planets.
-        SpawnPlanets(originPlanet, Iterations);
+        StartCoroutine(SpawnPlanets(originPlanet, Iterations));
     }
 
-    private void SpawnPlanets(Planet parent, int iterations) {
+    IEnumerator SpawnPlanets(Planet parent, int iterations) {
+        isSpawning = true;
+        yield return null;
+        Queue<Planet> spawnerQueue = new Queue<Planet>();
+        spawnerQueue.Enqueue(parent);
+        do {
+            for(int i = 0; i < PlanetChildCount; i++) {
+                while(validationAttempts > 0) {
+                    float radius = Random.Range(minRadiusFromPlanets + .0001f, maxRadiusFromLastPlanet);
+                    float angle = Random.Range(0f, 1f) * Mathf.PI * 2;
+                    float x = Mathf.Cos(angle) * radius;
+                    float y = Mathf.Sin(angle) * radius;
+
+                    Vector2 pos = new Vector2(spawnerQueue.Peek().Position.x + x, spawnerQueue.Peek().Position.y + y);
+
+                    if(Physics2D.OverlapCircle(pos, radius, planetLayerMask) == null) {
+                        
+                        Planet p = new Planet(pos, false, Distributor);
+                        GameObject newPlanet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                        DestroyImmediate(newPlanet.GetComponent<SphereCollider>());
+                        newPlanet.AddComponent<CircleCollider2D>();
+                        newPlanet.name = planetDict.Count.ToString();
+                        newPlanet.transform.position = pos;
+                        newPlanet.transform.parent = planetHierarchy.transform;
+                        newPlanet.transform.localScale *= Random.Range(minSizeMult, maxSizeMult);
+                        newPlanet.layer = LayerMask.NameToLayer("Planet");
+                        planetDict.Add(p, newPlanet);
+                        yield return new WaitForFixedUpdate();
+                        spawnerQueue.Enqueue(p);
+                        break;
+                    }
+                    else {
+                        Debug.Log("Hit planet");
+                    }
+                }
+                validationAttempts = maxValidationAttempts;
+            }
+            if(spawnerQueue.Count > 0) {
+                spawnerQueue.Dequeue();
+            }
+            iterations--;
+        }
+        while (iterations > 0);
+        isSpawning = false;
+    }
+
+    
+    private void SpawnPlanetsRecursive(Planet parent, int iterations) {
         //We're deep enough into our iteration max, we can safely return.
         if(iterations <= 0) {
             return;
@@ -83,16 +134,14 @@ public class PlanetGenerator : MonoBehaviour {
             //Attempt to generate a valid planet for this iteration attemps number of times before giving up.
             while (validationAttempts > 0) {
                 //Generate a location along the circumfrence of a circle origin parent.pos, radius randomly generated between min and max exclusive.
-                float radius = Random.Range(minRadiusFromPlanets+.0001f, maxRadiusFromLastPlanet);
+                float radius = Random.Range(minRadiusFromPlanets+(maxSizeMult*2)+.0001f, maxRadiusFromLastPlanet);
                 float angle = Random.Range(0f, 1f) * Mathf.PI * 2;
                 float x = Mathf.Cos(angle) * radius;
                 float y = Mathf.Sin(angle) * radius;
 
                 Vector2 pos = new Vector2(parent.Position.x + x, parent.Position.y + y);
                 //Validate that the planet is at least min distance from all other planets, continue to next attempt or fail out.
-                Collider[] colliders = Physics.OverlapSphere(pos, radius, planetLayerMask);
-
-                if (colliders.Length <= 1) {
+                if (Physics2D.OverlapCircle(pos, radius, planetLayerMask) == null) {
                     //We have vaildated, create a planet from the position.
                     Planet p = new Planet(pos, false, Distributor);
                     //Generate a planet with a random size, within constraints, give it a name and add to dict.
