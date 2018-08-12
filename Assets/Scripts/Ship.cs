67104d8d8ed03d52c7999507903a9dcd7cefe69d
JackWesleyNelson <nelsonw948@msn.com>
2018-08-12 00:29:19 -0600

    ship now can move to other planets, currently without checking for fuel levels, and orbits them upon arrival. Rotation when first arriving at the planet could be smoothed out a bit, but fine for now.

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour {
     public Ship Instance { get; private set; }
+    
+    private Vector3 orbitCenter;
+    private float orbitRadius = .5f;
+    private float orbitSpeed = 0.5f;
+
+    private bool isMoving = false;
+
     public List<ShipItem> ShipItems { get; private set; }
     public List<ShipItem> ShipItemsCatalogue { get; private set; }
     [SerializeField]
     ShipItemList ShipItemsCatalogueSerializable;
+    [SerializeField]
+    LayerMask planetLayerMask;
     public int InventoryItemSize { get; private set; } = 0;
     public int InventoryItemSizeMax { get; private set; } = 4;
     public float Fuel { get; private set; }
@@ -16,8 +25,8 @@ public class Ship : MonoBehaviour {
     public int Speed { get; private set; }
     public int ShipItemSize = 0;
     public int ShipItemSizeMax { get; private set; }
-    public float Time { get; private set; } = 0;
-    public int Currency { get; private set; } = 500;
+    public float GameTime { get; private set; } = 0;
+    public int Credits { get; private set; } = 500;    
 
     public void Start() {
         if(Instance == null) {
@@ -26,6 +35,7 @@ public class Ship : MonoBehaviour {
         if(this != Instance) {
             Destroy(this.gameObject);
         }
+        orbitCenter = new Vector3(0, 0, 0);
 
         ShipItems = new List<ShipItem>();
         ShipItemsCatalogue = new List<ShipItem>(ShipItemsCatalogueSerializable.shipItemList);
@@ -46,11 +56,32 @@ public class Ship : MonoBehaviour {
         }
         ShipItemSize = ShipItemSizeMax;
         Fuel = FuelMax;
+        transform.position += new Vector3(orbitRadius, 0, 0);
     }
 
     public void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, planetLayerMask);
+            if(hit.collider != null) {
+                isMoving = true;
+                orbitCenter = hit.transform.position;
+            }
+        }
+    }
+
+    public void FixedUpdate() {
+        
+        if (!isMoving) {
+            transform.RotateAround(orbitCenter, Vector3.forward, orbitSpeed);
+        }
+        else {
+            if (Vector3.Distance(transform.position, orbitCenter) < orbitRadius + .001) {
+                isMoving = false;
+                Quaternion rot = transform.rotation;
+                rot.z = 0;
+                transform.rotation = rot;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, orbitCenter + new Vector3(orbitRadius, 0, 0), orbitSpeed *2 * Time.deltaTime);
         }
     }
 
@@ -62,13 +93,13 @@ public class Ship : MonoBehaviour {
         float fuelNeeded = FuelConsumption * timeToTravel;
         if(Fuel >= fuelNeeded) {
             Fuel -= fuelNeeded;
-            Time += timeToTravel;
+            GameTime += timeToTravel;
             transform.position = new Vector3(p.Position.x, p.Position.y, transform.position.z);
         }
         else {
             float percentDistanceAchieved = Fuel / fuelNeeded;
             Fuel = 0;
-            Time += timeToTravel * percentDistanceAchieved;
+            GameTime += timeToTravel * percentDistanceAchieved;
             transform.position += new Vector3(dir.x, dir.y, 0) * percentDistanceAchieved;
         }
     }
@@ -78,13 +109,13 @@ public class Ship : MonoBehaviour {
     }
 
     public void AddCurrency(int amount) {
-        Currency += amount;
+        Credits += amount;
     }
 
     //Return true if the transaction completed, false otherwise.
     public bool RemoveCurrency(int amount) {
-        if(Currency >= amount) {
-            Currency -= amount;
+        if(Credits >= amount) {
+            Credits -= amount;
             return true;
         }
         return false;
